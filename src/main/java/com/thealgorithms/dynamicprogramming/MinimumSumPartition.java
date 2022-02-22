a47f37d61369a01648c20817200005695271dda6
Isabel Redtzer <isabel.redtzer@gmail.com>
2022-02-22 18:11:50 +0100

    Added coverage measurement for function subSeb() #9

--- src/main/java/com/thealgorithms/dynamicprogramming/MinimumSumPartition.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/MinimumSumPartition.java
@@ -16,25 +16,32 @@ Subset2 = {36, 40} ; sum of Subset2  = 76
  */
 public class MinimumSumPartition {
 
-    public static int subSet(int[] arr) {
+    public static int subSet(int[] arr, boolean[] checked) {
         int n = arr.length;
         int sum = getSum(arr);
         boolean[][] dp = new boolean[n + 1][sum + 1];
         for (int i = 0; i <= n; i++) {
+            checked[0] = true;
             dp[i][0] = true;
         }
         for (int j = 0; j <= sum; j++) {
+            checked[1] = true;
             dp[0][j] = false;
         }
 
         // fill dp array
         for (int i = 1; i <= n; i++) {
+            checked[2] = true;
             for (int j = 1; j <= sum; j++) {
+                checked[3] = true;
                 if (arr[i - 1] < j) {
+                    checked[4] = true;
                     dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
                 } else if (arr[i - 1] == j) {
+                    checked[5] = true;
                     dp[i][j] = true;
                 } else {
+                    checked[6] = true;
                     dp[i][j] = dp[i - 1][j];
                 }
             }
@@ -44,7 +51,9 @@ public class MinimumSumPartition {
         int[] index = new int[sum];
         int p = 0;
         for (int i = 0; i <= sum / 2; i++) {
+            checked[7] = true;
             if (dp[n][i]) {
+                checked[8] = true;
                 index[p++] = i;
             }
         }
@@ -81,8 +90,19 @@ public class MinimumSumPartition {
      * Driver Code
      */
     public static void main(String[] args) {
-        assert subSet(new int[]{1, 6, 11, 5}) == 1;
-        assert subSet(new int[]{36, 7, 46, 40}) == 23;
-        assert subSet(new int[]{1, 2, 3, 9}) == 3;
+        boolean[] checked = new boolean[9];
+
+        for(int i = 0; i < 9; i ++){
+            checked[i] = false;
+        }
+        subSet(new int[]{1, 6, 11, 5}, checked);
+        subSet(new int[]{36, 7, 46, 40}, checked);
+        subSet(new int[]{1, 2, 3, 9}, checked);
+        assert subSet(new int[]{1, 6, 11, 5}, checked) == 1;
+        assert subSet(new int[]{36, 7, 46, 40}, checked) == 23;
+        assert subSet(new int[]{1, 2, 3, 9}, checked) == 3;
+        for(int i = 0; i < 9; i ++){
+            System.out.println(checked[i]);
+        }
     }
 }
