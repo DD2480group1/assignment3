3a6fc81ff209825ca57de81b9bfa1c6084fb84d8
leren1 <98456022+leren1@users.noreply.github.com>
2022-02-23 00:27:04 +0100

    Fix issue #12. Refactor Gaussian from one big function to three smaller.

--- src/main/java/com/thealgorithms/maths/Gaussian.java
+++ src/main/java/com/thealgorithms/maths/Gaussian.java
@@ -4,44 +4,50 @@ import java.util.ArrayList;
 
 public class Gaussian {
 
-    public static ArrayList<Double> gaussian(int mat_size, ArrayList<Double> matrix){
-        int  i, j, step;
+    public static ArrayList<Double> gaussian(int mat_size, ArrayList<Double> matrix) {
         ArrayList<Double> answerArray = new ArrayList<Double>();
-
+        int i, j = 0;
 
         double[][] mat = new double[mat_size + 1][mat_size + 1];
         double[][] x = new double[mat_size][mat_size + 1];
 
-        System.out.println("Enter value of the matrix");
-        System.out.println(' ');
+        // Values from arraylist to matrix
         for (i = 0; i < mat_size; i++) {
             for (j = 0; j <= mat_size; j++) {
-                mat[i][j] = matrix.get(i) ;
+                mat[i][j] = matrix.get(i);
             }
         }
 
-        // perform Gaussian elimination
+        mat = gaussianElimination(mat_size, i, mat);
+        answerArray = valueOfGaussian(mat_size, x, mat);
+        return answerArray;
+    }
+
+    // Perform Gaussian elimination
+    public static double[][] gaussianElimination(int mat_size, int i, double[][] mat) {
+        int step = 0;
         for (step = 0; step < mat_size - 1; step++) {
             for (i = step; i < mat_size - 1; i++) {
                 double a = (mat[i + 1][step] / mat[step][step]);
 
-                for (j = step; j <= mat_size; j++) {
+                for (int j = step; j <= mat_size; j++) {
                     mat[i + 1][j] = mat[i + 1][j] - (a * mat[step][j]);
                 }
             }
         }
+        return mat;
+    }
 
-        System.out.println("Matrix using Gaussian Elimination method: ");
-        System.out.println(" ");
+    // calcilate the x_1, x_2,... values of the gaussian and save it in an arraylist.
+    public static ArrayList<Double> valueOfGaussian(int mat_size, double[][] x, double[][] mat) {
+        ArrayList<Double> answerArray = new ArrayList<Double>();
+        int i, j;
+
         for (i = 0; i < mat_size; i++) {
             for (j = 0; j <= mat_size; j++) {
                 x[i][j] = mat[i][j];
-                System.out.print(mat[i][j] + " ");
             }
-            System.out.println();
         }
-        System.out.println("Value of the Gaussian Elimination method: ");
-        System.out.println(" ");
 
         for (i = mat_size - 1; i >= 0; i--) {
             double sum = 0;
@@ -54,11 +60,8 @@ public class Gaussian {
             } else {
                 x[i][i] = (x[i][mat_size] - sum) / (x[i][i]);
             }
-            System.out.print("x" + i + "=" + x[i][i]);
-            System.out.println(" ");
             answerArray.add(x[i][j]);
         }
         return answerArray;
     }
 }
-
