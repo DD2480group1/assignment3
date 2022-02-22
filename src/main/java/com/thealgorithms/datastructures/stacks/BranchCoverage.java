df1016ca7855fb1f0a9a888cf71ed3f22cfa696d
vluthman <vluthman@kth.se>
2022-02-22 16:53:36 +0100

    Add history of paths taken #4

--- src/main/java/com/thealgorithms/datastructures/stacks/BranchCoverage.java
+++ src/main/java/com/thealgorithms/datastructures/stacks/BranchCoverage.java
@@ -1,14 +1,31 @@
 package com.thealgorithms.datastructures.stacks;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class BranchCoverage {
 	private static Map<String, Integer> visitCount = new HashMap<>();
+	private static List<List<String>> paths = new ArrayList<>(new ArrayList<>());
 
-	static void visit(String id) {
+	static void incrementCount(String id) {
 		int count = visitCount.getOrDefault(id, 0) + 1;
 		visitCount.put(id, count);
+	}
+
+	static void appendPath(String id) {
+		List<String> last = paths.get(paths.size()-1);
+		last.add(id);
+	}
+
+	static void visit(String id) {
+		incrementCount(id);
+		appendPath(id);
+	}
+
+	static void startNewPath() {
+		paths.add(new ArrayList<>());
 	}
 
 	static int visitCount(String id) {
