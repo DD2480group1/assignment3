f4906a140a4a6b58896bb8c73d0c8fc03d200cb4
mayurims <mayurims@users.noreply.github.com>
2022-02-24 05:35:00 +0800

    Test for Area and Factorial
    Tests for Area.java and Factorial.java

--- src/test/java/com/thealgorithms/maths/FactorialTest.java
+++ src/test/java/com/thealgorithms/maths/FactorialTest.java
@@ -1,6 +1,10 @@
 package com.thealgorithms.maths;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+
 
 public class FactorialTest {
     // Testing the factorial function
@@ -11,8 +15,16 @@ public class FactorialTest {
     }
 
     // Testing the Exception that function won't return an answer for negative answers
+
     @Test
-    void testException() throws Exception{
-        Factorial.factorial(-15);
-    } 
+    public void testException1()  {
+        assertThrows(IllegalArgumentException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                Factorial.factorial(-15);
+            }
+        });
+    }
+
+
 }
